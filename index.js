ae9d7fe628157d5d8a1a7f8ea28e53e95f9e687c
farah0552001 <114291029+farah0552001@users.noreply.github.com>
2022-11-08 16:06:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function mooncakeSays (message){
+    let mooncake = '(0.0) ';
+    
+    if (!message){
+       return' ${mooncake) chookity?';
+    }
+    
+       return ' ${mooncake} ${essage)';
+    }
+    module.exports.mooncakeSays = mooncakeSays;
