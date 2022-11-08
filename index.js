66c6f6b411527ea5e678c42e8ece96ce5faf02ec
farah0552001 <114291029+farah0552001@users.noreply.github.com>
2022-11-08 16:33:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function mooncakeSays (message){
-    let mooncake = '(0.0) ';
+    let mooncake = '(0.o) ';
     
     if (!message){
        return' ${mooncake) chookity?';
     }
     
-       return ' ${mooncake} ${essage)';
+       return ' ${mooncake} ${essage)!';
     }
     module.exports.mooncakeSays = mooncakeSays;
